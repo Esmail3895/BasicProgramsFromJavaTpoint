61f69ffba2144db4179e11bab1edcf754b400f80
Esmail3895 <esmailpaltasingh123@gmail.com>
2021-10-13 13:28:52 +0530

    LeftTriangleStarPattern.java file modified.

--- JavaBasicProgramsFromJavaTpoint/src/com/javatpoint/basicprograms/LeftTriangleStarPattern.java
+++ JavaBasicProgramsFromJavaTpoint/src/com/javatpoint/basicprograms/LeftTriangleStarPattern.java
@@ -5,10 +5,20 @@ public class LeftTriangleStarPattern {
 	public static void main(String[] args) {
 		//count of rows and columns
 		int row = 5;
-		//first for loop for rows
-		for(int i=0; i<row; i++) {
+		//i for rows j for columns
+		int i, j;
+		//outer for loop works for rows
+		for(i=0; i<row; i++) {
+			
+			//inner for loop works for space
+			for(j=2*(row-i); j>=0; j--) {
+				//prints space between two stars 
+				System.out.print(" ");
+			}
+			
 			//inner for loop for columns
-			for(int j=i; j<=i; j++) {
+			for(j=0; j<=i; j++) {
+				//prints star
 				System.out.print("* ");
 			}
 			//print new line
